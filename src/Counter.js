cee26e6f5fe857a64028769ff7d6b9693b4b3434
abhiking8055 <abhiking8055@gmail.com>
2023-08-03 00:17:57 +0530

    message

---
+++ src/Counter.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./counter.css";
+const Counter = () => {
+  const [num, setnum] = useState(0);
+
+  function increment() {
+    setnum((a) => a + 1);
+  }
+
+  function decrement() {
+    
+    if (num <= 0) {
+      setnum((a) => 0);
+      window.alert("Value cannot be less than 0")
+    }
+    else
+    setnum((a) => a - 1);
+  }
+
+  function reset() {
+    setnum((a) => 0);
+  }
+  return (
+    <>
+      <div className="abc">
+        <button onClick={() => decrement()}>-</button>
+        {<h1>{num}</h1>}
+
+        <button onClick={() => increment()}>+</button>
+        <button onClick={() => reset()}>Reset</button>
+      </div>
+    </>
+  );
+};
+export default Counter;
